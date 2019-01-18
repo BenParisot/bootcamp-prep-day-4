fa4e9ab7096ff7856a6cfd6bb56c0753c7df8ec6
Ben Parisot <me@benparisot.com>
2019-01-17 19:54:56 -0800

    clean up code

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 // eslint-disable-next-line
 function greet() {
-    // console.log('greet function called');
-    //get the inputs by id (first, last)
     var firstName = document.getElementById('first-name');
     var lastName = document.getElementById('last-name');
-    // console.log(firstName.value, lastName.value);
-    //get values out of inputs
+    
     var fullName = firstName.value + ' ' + lastName.value;
-    //make the greeting 
+    
     var greeting = 'Why hello there ' + fullName + '!';
-    //display the greeting
+    
     var greeter = document.getElementById('greeting');
-    console.log(greeter);
-    greeter.textContent = greeting; //this is taking the content of that p element and replacing it with the new variable value
+    
+    greeter.textContent = greeting; //this is taking the content of that id=greeting p element and replacing it with the new variable value
 
 
 }
