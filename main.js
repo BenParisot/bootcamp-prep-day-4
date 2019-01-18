844633b122f761e89abfdf1c2cd8a9afe21d7974
Ben Parisot <me@benparisot.com>
2019-01-17 19:45:52 -0800

    make greeting function

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 // eslint-disable-next-line
 function greet() {
-    console.log('greet function called');
+    // console.log('greet function called');
     //get the inputs by id (first, last)
-
+    var firstName = document.getElementById('first-name');
+    var lastName = document.getElementById('last-name');
+    // console.log(firstName.value, lastName.value);
     //get values out of inputs
-
+    var fullName = firstName.value + ' ' + lastName.value;
     //make the greeting 
-
+    var greeting = 'Why hello there ' + fullName + '!';
     //display the greeting
 
+
+    console.log(greeting);
     //remove the prompt
 
 }
